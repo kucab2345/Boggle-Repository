c15cbdb6a46910ce2028ce99bbe4d8226dca7656
Henry Kucab <kucab2345@gmail.com>
2016-03-27 18:15:08 -0600

    Drafted DataModel.cs -Henry

---
+++ BoggleService/BoggleService/DataModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Boggle
+{
+    public class DataModel 
+    {
+        public class GameStatus
+        {
+            public string GameState { get; set; }
+            public string Board { get; set; }
+            public string TimeLimit { get; set; }
+            public string TimeLeft { get; set; }
+            
+        }
+        public class UserInfo
+        {
+            public string Nickname { get; set; }
+            public string Score { get; set; }
+            public string userToken { get; set; }
+        }
+        public class GameJoin
+        {
+            public string gameTime { get; set; }
+        }
+        public class UserGame
+        {
+            public string currentWord { get; set; }
+        }
+    }
+}

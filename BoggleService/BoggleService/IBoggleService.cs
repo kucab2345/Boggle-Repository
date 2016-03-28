812184ce5bf1230e903232e21abef268fba90c89
Henry Kucab <kucab2345@gmail.com>
2016-03-27 18:03:50 -0600

    No changes?

--- BoggleService/BoggleService/IBoggleService.cs
+++ BoggleService/BoggleService/IBoggleService.cs
@@ -13,41 +13,6 @@ namespace Boggle
         /// </summary>
         [WebGet(UriTemplate = "/api")]
         Stream API();
-
-        [WebInvoke(Method = "POST", UriTemplate = "/users")]
-        string RegisterUser(UserInfo user);
-
-        [WebInvoke(Method = "POST", UriTemplate = "/games")]
-        string JoinGame(GameJoinInfo info);
-
-        [WebInvoke(Method = "PUT", UriTemplate = "/games")]
-
-        void CancelGame(string userToken);
-
-        [WebInvoke(Method = "PUT", UriTemplate = "/games/{GameID}")]
-        string playWord(UserGame words, string GameID);
-
-        [WebGet(UriTemplate = "/games/{GameID")]
-        string getGamestatus(string GameID);
-
-        [WebGet(UriTemplate = "//games/{GameID")]
-        string GetAllItems(bool completedOnly, string userID);
-
-
-
-
-        /// <summary>
-        /// Demo.  You can delete this.
-        /// </summary>
-        /// <param name="n"></param>
-        /// <returns></returns>
-        [WebGet(UriTemplate = "/numbers?length={n}")]
-        IList<int> Numbers(string n);
-
-        /// <summary>
-        /// Demo.  You can delete this.
-        /// </summary>
-        [WebInvoke(Method = "POST", UriTemplate = "/first")]
-        int GetFirst(IList<int> list);
+        
     }
 }

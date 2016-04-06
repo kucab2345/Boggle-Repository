c37a7991d0b6ef2b30ac1c0c4a37040f5ea6e5e4
Ryan Steele <intangiblehawk@gmail.com>
2016-04-05 19:19:39 -0600

    Refixed join game -Ryan

--- BoggleService/BoggleService/BoggleService.svc.cs
+++ BoggleService/BoggleService/BoggleService.svc.cs
@@ -680,13 +680,14 @@ namespace Boggle
 
                     // Here we are executing an insert command, but notice the "output inserted.ItemID" portion.  
                     // We are asking the DB to send back the auto-generated ItemID.
-                    using (SqlCommand command = new SqlCommand("Declare @id int Update Top (1) Games Set Player2 = 2, TimeLimit = 40, @id=GameID  where Player2 is Null and Player1 is not null select @id", conn, trans))
+                    using (SqlCommand command = new SqlCommand("Declare @id int Update Top (1) Games Set Player2 = @Player, TimeLimit = @Time, Board = @Board,  StartTime = @StartTime, @id=GameID  where Player2 is Null and Player1 is not null select @id", conn, trans))
                     {
                         ///test
                         ///This is another test
-
+                        BoggleBoard board = new BoggleBoard();
                         command.Parameters.AddWithValue("@Player", info.UserToken);
                         command.Parameters.AddWithValue("@Time", DateTime.Now);
+                        command.Parameters.AddWithValue("@Board", board.ToString());
 
 
                         using (SqlDataReader reader = command.ExecuteReader())

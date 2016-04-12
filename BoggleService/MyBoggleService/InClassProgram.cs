cf4ba485b551ae0c8d239bcfd4c9fd915083ceb4
Henry Kucab <kucab2345@gmail.com>
2016-04-12 17:36:39 -0600

    Removed player class at bottom -Henry

--- BoggleService/MyBoggleService/InClassProgram.cs
+++ BoggleService/MyBoggleService/InClassProgram.cs
@@ -249,10 +249,4 @@ namespace SimpleWebServer
         {
         }
     }
-
-    public class Person
-    {
-        public String Name { get; set; }
-        public String Eyes { get; set; }
-    }
 }

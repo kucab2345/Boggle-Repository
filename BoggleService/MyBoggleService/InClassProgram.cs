2e3124b4b7853f8f229c4516582942d642e17ac9
Ryan Steele <intangiblehawk@gmail.com>
2016-04-12 17:55:23 -0600

    Fixed api -Ryan

--- BoggleService/MyBoggleService/InClassProgram.cs
+++ BoggleService/MyBoggleService/InClassProgram.cs
@@ -66,7 +66,7 @@ namespace SimpleWebServer
                     
                     URLAddress = m.Groups[2].Value;
                 }
-                if (MethodType.Equals("GET") && URLAddress == null)
+                if (MethodType.Equals("GET") && URLAddress == "/")
                 {
                     getAPI();
                 }
@@ -98,6 +98,7 @@ namespace SimpleWebServer
             ss.BeginSend("\r\n", Ignore, null);
             ss.BeginSend(API, (ex, py) => { ss.Shutdown(); }, null);
         }
+
         private void GetContent(string s)
         {
             Regex r = new Regex(@"^/BoggleService.svc/games/(\d+)$");
